7d813510904933e9f9215c8989701193e1cdd071
Naoual-Khelloufi <naoualKhelloufiPro@gmail.com>
2025-09-09 17:56:23 +0100

    structure the code

--- core/rag.py
+++ core/rag.py
@@ -1,4 +1,3 @@
-# RAG pipeline (LangChain)
 """RAG pipeline core functionality - simplified version."""
 import logging
 from langchain_core.runnables import RunnablePassthrough
@@ -14,8 +13,8 @@ class RAGPipeline:
         Initialize with pre-configured components.
         
         Args:
-            vector_store: Your VectorStore instance from embeddings.py
-            llm_manager: Your LLMManager instance from llm.py
+            vector_store: our VectorStore instance from embeddings.py
+            llm_manager: our LLMManager instance from llm.py
         """
         self.vector_store = vector_store
         self.llm_manager = llm_manager
@@ -39,40 +38,20 @@ class RAGPipeline:
         try:
             return self.vector_store.similarity_search(question, k=4)
         except Exception:
-            # si la base vectorielle n'est pas initialisée
+            # if the vectore database is not initiated
             return []
-
-    #def get_response(self, question: str) -> str:
-        """
-        Get answer to user question.
-        
-        Args:
-            question: User query string
-            
-        Returns:
-            Generated answer
-        """
-    #    try:
-    #        logger.info(f"Processing question: {question[:50]}...")
-    #        return self.chain.invoke(question)
-    #    except Exception as e:
-    #        logger.error(f"Response generation failed: {e}")
-    #        raise RuntimeError("Please check your input and try again")
 
     def get_response(self, question: str) -> str:
         """
         Get answer to user question.
-        - Si aucun document n'est disponible/retourné -> réponse générale
-        - Sinon -> RAG (chaîne existante)
+        - if non doc is available/return -> general responce
+        - else -> RAG (chain exist)
         """
         try:
-            # 1) On tente la recherche
             docs = self._retrieve_context(question)
             if not docs:   # Fallback simple
-                # Réponse générale (sans contexte)
                 return self.llm_manager.generate_general(question, max_tokens=600)
 
-            # 2) Sinon: on passe par la chaîne RAG (comme avant)
             logger.info(f"Processing question: {question[:50]}...")
             return self.chain.invoke(question)
 
